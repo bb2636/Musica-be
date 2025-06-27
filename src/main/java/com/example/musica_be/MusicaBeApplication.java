63260da3fbfbd44725e38bbdaa37308c5bbed5de
bb2636 <leesubin0322@gmail.com>
2025-06-27 11:54:26 +0900

    프로젝트 구조 설정

---
+++ src/main/java/com/example/musica_be/MusicaBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.musica_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicaBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicaBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/musica_be/MusicaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musica_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
