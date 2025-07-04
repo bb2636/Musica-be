2c5eb87d895bc9967902e73371cc0a3376e64ef9
조윤빈 <bamwing19@gmail.com>
2025-07-04 11:34:13 +0900

    찜 도메인 추가

---
+++ src/main/java/com/example/musica_be/domain/content/Review.java
@@ -0,0 +1,37 @@
+package com.example.musica_be.domain.content;
+
+import com.example.musica_be.domain.user.User;
+import jakarta.persistence.*;
+        import java.time.LocalDateTime;
+
+@Entity
+public class Review {
+
+    @Id
+    @Column(name = "review_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer reviewId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+//    @ManyToOne
+//    @JoinColumn(name = "class_id", nullable = false)
+//    private ClassEntity classEntity;
+    @Column(name = "class_id", nullable = false)
+    private Integer classId;
+
+    @Column(nullable = false)
+    private Integer rating;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String comment;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+
+    // Getters and Setters
+
+}
--- src/main/java/com/example/musica_be/domain/content/Wishlist.java
+++ src/main/java/com/example/musica_be/domain/content/Wishlist.java
@@ -1,19 +1,19 @@
-package com.example.musica_be.domain;
+package com.example.musica_be.domain.content;
 
 import jakarta.persistence.*;
-        import java.time.LocalDateTime;
+import java.time.LocalDateTime;
 
 @Entity
-public class Review {
+public class Wishlist {
 
     @Id
-    @Column(name = "review_id")
+    @Column(name = "wishlist_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer reviewId;
+    private Integer wishlistId;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+    private com.example.musica_be.domain.user.User user;
 
 //    @ManyToOne
 //    @JoinColumn(name = "class_id", nullable = false)
@@ -21,15 +21,8 @@ public class Review {
     @Column(name = "class_id", nullable = false)
     private Integer classId;
 
-    @Column(nullable = false)
-    private Integer rating;
-
-    @Column(nullable = false, columnDefinition = "TEXT")
-    private String comment;
-
-    @Column(name = "created_at", nullable = false)
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
-
 
     // Getters and Setters
 
