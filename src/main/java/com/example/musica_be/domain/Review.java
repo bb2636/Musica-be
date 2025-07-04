adac9960fc460ee13924c0fffd576c1c03fbaa0b
조윤빈 <bamwing19@gmail.com>
2025-07-04 10:25:01 +0900

    feat: 리뷰 도메인 추가

---
+++ src/main/java/com/example/musica_be/domain/Review.java
@@ -0,0 +1,36 @@
+package com.example.musica_be.domain;
+
+import jakarta.persistence.*;
+        import java.time.LocalDateTime;
+
+@Entity
+public class Review {
+
+    @Id
+    @Column(name = "review_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer reviewId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+//    @ManyToOne
+//    @JoinColumn(name = "class_id", nullable = false)
+//    private ClassEntity classEntity;
+    @Column(name = "class_id", nullable = false)
+    private Integer classId;
+
+    @Column(nullable = false)
+    private Integer rating;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String comment;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+
+    // Getters and Setters
+
+}
