e328db3e6c09f67d015108b1dd66c54af38ed7fd
bb2636 <leesubin0322@gmail.com>
2025-07-08 16:06:19 +0900

    마이페이지 용 조회 추가(후기, 찜)

--- src/main/java/com/example/musica_be/controller/UserController.java
+++ src/main/java/com/example/musica_be/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.example.musica_be.controller;
 
+import com.example.musica_be.domain.Review;
+import com.example.musica_be.domain.Wishlist;
+import com.example.musica_be.domain.user.User;
 import com.example.musica_be.dto.user.UpdateUserReqDto;
+import com.example.musica_be.security.JwtTokenService;
 import com.example.musica_be.service.user.UserService;
 import com.example.musica_be.dto.user.LoginReqDto;
 import com.example.musica_be.dto.user.RegisterReqDto;
@@ -8,8 +12,11 @@ import com.example.musica_be.dto.user.UserResDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -17,6 +24,7 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final JwtTokenService jwtTokenService;
 
     // 회원가입
     @PostMapping("/users/register")
@@ -62,15 +70,60 @@ public class UserController {
     }
 
     // 회원 정보 수정
+    @PreAuthorize("hasRole('USER')")
     @PatchMapping("/users/{userId}")
     public ResponseEntity<UserResDto> updateUser(
             @PathVariable Long userId,
             @RequestBody UpdateUserReqDto updateUserReqDto) {
         try {
-            UserResDto updatedUser = userService.updateUser(userId, updateUserReqDto);
+            UserResDto updatedUser = userService.updateUserInfo(userId, updateUserReqDto);
             return ResponseEntity.ok(updatedUser);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new UserResDto(e.getMessage()));
         }
     }
+
+    //사용자 마이페이지 정보 조회
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/users/mypage")
+    public ResponseEntity<UserResDto> getUserMypage(@AuthenticationPrincipal User user) {
+        // 사용자 정보 가져오기
+        UserResDto userResDto = new UserResDto(user);
+        return ResponseEntity.ok(userResDto);
+    }
+
+//    @PreAuthorize("hasRole('USER')")
+//    @GetMapping("/users/mypage/enrollments")
+//    public ResponseEntity<List<Enrollment>> getEnrollments(@AuthenticationPrincipal User user) {
+//        List<Enrollment> enrollments = userService.getCurrentEnrollments(user.getId());
+//        return ResponseEntity.ok(enrollments);
+//    }
+//
+//    @PreAuthorize("hasRole('USER')")
+//    @GetMapping("/users/mypage/payments")
+//    public ResponseEntity<List<Payment>> getPaymentHistory(@AuthenticationPrincipal User user) {
+//        List<Payment> payments = userService.getPaymentHistory(user.getId());
+//        return ResponseEntity.ok(payments);
+//    }
+    //찜목록 조회
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/users/mypage/wishlist")
+    public ResponseEntity<List<Wishlist>> getWishlist(@AuthenticationPrincipal User user) {
+        List<Wishlist> wishlist = userService.getWishlist(user.getId());
+        return ResponseEntity.ok(wishlist);
+    }
+    //후기 목록 조회
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/users/mypage/reviews")
+    public ResponseEntity<List<Review>> getReviews(@AuthenticationPrincipal User user) {
+        List<Review> reviews = userService.getReviews(user.getId());
+        return ResponseEntity.ok(reviews);
+    }
+//
+//    @PreAuthorize("hasRole('USER')")
+//    @GetMapping("/users/mypage/questions")
+//    public ResponseEntity<List<Question>> getUserQuestions(@AuthenticationPrincipal User user) {
+//        List<Question> questions = userService.getUserQuestions(user.getId());
+//        return ResponseEntity.ok(questions);
+//    }
 }
--- src/main/java/com/example/musica_be/repository/review/ReviewRepository.java
+++ src/main/java/com/example/musica_be/repository/review/ReviewRepository.java
@@ -4,8 +4,11 @@ import com.example.musica_be.domain.Review;
 import com.example.musica_be.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
     Optional<Review> findByReviewIdAndUser(Integer reviewId, User user); // 본인 확인용
+
+    List<Review> findByUserId(Long userId);
 }
