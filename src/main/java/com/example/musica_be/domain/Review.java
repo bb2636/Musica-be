1b16d062b70d2eb91fb6d9e31f732a58c11562d8
조윤빈 <bamwing19@gmail.com>
2025-07-08 19:23:23 +0900

    feat: 리뷰 기능 추가 (Controller, Service, DTO, Repository, Entity)

--- src/main/java/com/example/musica_be/domain/Review.java
+++ src/main/java/com/example/musica_be/domain/Review.java
@@ -18,7 +18,7 @@ public class Review {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer reviewId;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
--- src/main/java/com/example/musica_be/dto/review/ReviewResponseDto.java
+++ src/main/java/com/example/musica_be/dto/review/ReviewResponseDto.java
@@ -10,4 +10,10 @@ public class ReviewResponseDto {
     private String status;
     private String message;
     private Integer reviewId;
+    private String name;
+    private int rating;
+    private String comment;
+    private int progress;
+    private boolean isAuthor; // 작성자 변별목적
+    private String createdAt; // 후기 최신순 정렬 목적
 }
