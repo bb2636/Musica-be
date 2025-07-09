cea27b94ef055c09f1396c083ba66f4dcce5b23a
hhs5486 <hhs5486@naver.com>
2025-07-09 11:56:38 +0900

    Cart Service

--- src/main/java/com/example/musica_be/domain/cart/CartItem.java
+++ src/main/java/com/example/musica_be/domain/cart/CartItem.java
@@ -18,7 +18,7 @@ public class CartItem {
   private Cart cart;
 
   @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "classes_id", nullable = false)
+  @JoinColumn(name = "class_id", nullable = false)
   private Classes classes;
 
   private Long quantity;
