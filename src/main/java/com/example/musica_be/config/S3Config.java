f9c4f996e931f987be059d4b162515135f8bacb6
artp <dky7034@naver.com>
2025-07-22 17:38:18 +0900

    s3 업로드 오류 수정 - S3Config.java

--- src/main/java/com/example/musica_be/config/S3Config.java
+++ src/main/java/com/example/musica_be/config/S3Config.java
@@ -3,11 +3,19 @@ package com.example.musica_be.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class S3Config {
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
 
     @Value("${cloud.aws.region.static}")
     private String region;
@@ -16,6 +24,11 @@ public class S3Config {
     public S3Presigner s3Presigner() {
         return S3Presigner.builder()
             .region(Region.of(region))
-            .build(); // credentialsProvider 기본 사용
+            .credentialsProvider(
+                StaticCredentialsProvider.create(
+                    AwsBasicCredentials.create(accessKey, secretKey)
+                )
+            )
+            .build();
     }
 }
